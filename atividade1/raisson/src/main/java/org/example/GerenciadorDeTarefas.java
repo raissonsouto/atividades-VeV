634d1e72577d36abcfcb83e555f3daf85cbd0d8a
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2023-08-20 14:55:30 -0300

    criando gerenciador de tarefas.

--- atividade1/raisson/src/main/java/org/example/GerenciadorDeTarefas.java
+++ atividade1/raisson/src/main/java/org/example/GerenciadorDeTarefas.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorDeTarefas {
+    private List<Tarefa> tarefas;
+
+    public GerenciadorDeTarefas() {
+        tarefas = new ArrayList<>();
+    }
+
+    public void criarTarefa(String titulo, String descricao, String dataDeVencimento, Prioridade prioridade) {
+        Tarefa tarefa = new Tarefa(titulo, descricao, dataDeVencimento, prioridade);
+        tarefas.add(tarefa);
+    }
+
+    public Tarefa getTarefa(String titulo) {
+        for (Tarefa tarefa: tarefas) {
+            if (tarefa.getTitulo().equals(titulo)) {
+                return tarefa;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Tarefa> getAllTarefas() {
+        return tarefas;
+    }
+
+    public void deleteTarefa(String titulo) {
+        for (Tarefa tarefa: tarefas) {
+            if (tarefa.getTitulo().equals(titulo)) {
+                tarefas.remove(tarefa);
+                break;
+            }
+        }
+    }
 }
