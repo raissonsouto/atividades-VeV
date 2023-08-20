6ebed115f2106f9d06ced13c4c9edea35e8ae6d8
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2023-08-20 14:27:31 -0300

    adicionando testes

--- atividade1/raisson/src/test/java/org/example/GerenciadorDeTarefasTests.java
+++ atividade1/raisson/src/test/java/org/example/GerenciadorDeTarefasTests.java
@@ -1,27 +1,103 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class GerenciadorDeTarefasTests {
+
+    @BeforeAll
+    public static void createGerenciador() {
+        GerenciadorDeTarefas gerenciadorDeTarefas = new GerenciadorDeTarefas();
+    }
+
     @Test
     public void testCriarNovaTarefa() {
-        String titulo = "";
-        String descricao = "";
-        String dataDeVencimento = "";
-        String prioridade = "";
         try {
-            GerenciadorDeTarefas gerenciadorDeTarefas = new GerenciadorDeTarefas(
+            String titulo = "";
+            String descricao = "";
+            String dataDeVencimento = "";
+            String prioridade = "";
+
+            gerenciadorDeTarefas.criarTarefa(
                     titulo,
                     descricao,
                     dataDeVencimento,
-                    prioridade);
+                    prioridade
+            );
 
-            assertTrue(true);
+            Assertions.assertTrue(true);
+
         } catch (Exception e) {
-            fail("Teste falhou pois classe não foi criada com sucesso.");
+            Assertions.fail("Teste falhou pois classe não foi criada com sucesso.");
         }
     }
+
+    @Test
+    public void testUpdateTitulo() {
+        String titulo = "";
+        Tarefa tarefa = gerenciadorDeTarefas.getTarefa(titulo);
+        tarefa.setTitulo(titulo);
+
+        Assertions.assertEquals(titulo, tarefa.getTitulo());
+    }
+
+    @Test
+    public void testUpdateDescricao() {
+        String descricao = "";
+
+        Tarefa tarefa = gerenciadorDeTarefas.getTarefa(titulo);
+        tarefa.setDescricao(descricao);
+
+        Assertions.assertEquals(descricao, tarefa.getDescricao());
+    }
+
+    @Test
+    public void testUpdateDataDeVencimento() {
+        String dataDeVencimento = "";
+
+        Tarefa tarefa = gerenciadorDeTarefas.getTarefa(titulo);
+        tarefa.setDataDeVencimento(dataDeVencimento);
+
+        Assertions.assertEquals(dataDeVencimento, tarefa.getDataDeVencimento());
+    }
+
+    @Test
+    public void testDefinirPrioridadeAlta() {
+        Tarefa tarefa = gerenciadorDeTarefas.getTarefa(titulo);
+        tarefa.setPrioridade(Prioridade.ALTA);
+
+        Assertions.assertNotEquals(Prioridade.ALTA, tarefa.getPrioridade());
+    }
+
+    @Test
+    public void testDefinirPrioridadeMedia() {
+        Tarefa tarefa = gerenciadorDeTarefas.getTarefa(titulo);
+        tarefa.setPrioridade(Prioridade.MEDIA);
+
+        Assertions.assertNotEquals(Prioridade.MEDIA, tarefa.getPrioridade());
+    }
+
+    @Test
+    public void testDefinirPrioridadeBaixa() {
+        Tarefa tarefa = gerenciadorDeTarefas.getTarefa(titulo);
+        tarefa.setPrioridade(Prioridade.BAIXA);
+
+        Assertions.assertNotEquals(Prioridade.BAIXA, tarefa.getPrioridade());
+    }
+
+    @Test
+    public void testDeleteTarefa() {
+        int tamanhoDaLista = gerenciadorDeTarefas.length();
+        gerenciadorDeTarefas.deleteTarefa(titulo);
+        int novoTamanhoDaLista = gerenciadorDeTarefas.length();
+
+        Assertions.assertEquals(tamanhoDaLista - 1, novoTamanhoDaLista);
+    }
+
+    @Test
+    public void testGetAllTarefas() {
+        Assertions.assertEquals("", gerenciadorDeTarefas.getAllTarefas()); // nao consigo imaginar o formato de saida
+    }
+
 }
